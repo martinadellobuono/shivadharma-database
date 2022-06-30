b30cf7e0b63ebec63913263967030075ae7e75ee
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2022-06-30 10:27:30 +0200

    upload txt and docx

--- routes/addFile.js
+++ routes/addFile.js
@@ -13,88 +13,101 @@ const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post("/addFile/:id", async (req, res) => {
-    var idEdition = req.params.id.split("/").pop().split("-")[0];
-    var idEditor = req.params.id.split("/").pop().split("-")[1];
-    var form = formidable();
-    form.parse(req);
-    form.on("fileBegin", (name, file) => {
-        file.path = `${__dirname}/../uploads/${file.name}`;
-    });
-    form.on("file", async (name, file) => {
-        /* convert docx to html */
-        if (file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
-            mammoth.convertToHtml({ path: file.path })
-                .then((result) => {
-                    try {
-                        fs.access(file.path, fs.F_OK, () => {
-                            fs.writeFile(file.path, result.value, "utf8", (err) => {
-                                if (err) {
-                                    console.log("Error related to rewriting the file: " + err);
-                                } else {
-                                    console.log("The file has been overwritten");
-                                };
+const { body, validationResult } = require("express-validator");
+
+router.post("/addFile/:id",
+    async (req, res) => {
+        var idEdition = req.params.id.split("/").pop().split("-")[0];
+        var idEditor = req.params.id.split("/").pop().split("-")[1];
+        var form = formidable();
+        form.parse(req);
+        form.on("fileBegin", (name, file) => {
+            file.path = `${__dirname}/../uploads/${file.name}`;
+        });
+        form.on("file", async (name, file) => {
+            /* convert to html */
+            /* docx > html */
+            if (file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+                mammoth.convertToHtml({ path: file.path })
+                    .then((result) => {
+                        try {
+                            fs.access(file.path, fs.F_OK, () => {
+                                fs.writeFile(file.path, result.value, "utf8", (err) => {
+                                    if (err) {
+                                        console.log("Error related to rewriting the file: " + err);
+                                    } else {
+                                        console.log("The file has been overwritten");
+                                    };
+                                });
+                                fs.rename(file.path, file.path + ".html", (err) => {
+                                    if (err) {
+                                        console.log("Error related to renaming the file: " + err);
+                                    } else {
+                                        console.log("The file has been renamed: " + file.name);
+                                    };
+                                });
                             });
-                            fs.rename(file.path, file.path + ".html", (err) => {
-                                if (err) {
-                                    console.log("Error related to renaming the file: " + err);
-                                } else {
-                                    console.log("The file has been renamed: " + file.name);
-                                };
-                            });
-                        });
-                    } catch (error) {
-                        console.log("Error in converting the file: " + error);
+                        } catch (error) {
+                            console.log("Error in converting the file: " + error);
+                        };
+                    })
+                    .done();
+            /* any format > html */
+            } else {
+                fs.rename(file.path, file.path + ".html", (err) => {
+                    if (err) {
+                        console.log("Error related to renaming the file: " + err);
+                    } else {
+                        console.log("The file has been renamed: " + file.name);
                     };
-                })
-                .done();
-        };
-        /* post the file */
-        var fileNewName = file.name + ".html";
-        const session = driver.session();
-        try {
-            await session.writeTransaction(tx => tx
-                .run(
-                    `
-                    MATCH (author:Author)<-[w:WRITTEN_BY]-(work:Work)-[r:HAS_MANIFESTATION]->(edition:Edition)-[e:EDITED_BY]->(editor:Editor)
-                    WHERE id(editor) = ${idEditor} AND id(editor) = ${idEditor}
-                    OPTIONAL MATCH (edition)-[p:PUBLISHED_ON]->(date:Date)
-                    MERGE (file:File {name: $file})
-                    MERGE (file)-[pr:PRODUCED_BY]->(editor)
-                    RETURN work.title, edition.title, author.name, editor.name, date.on, file.name
-                    `, { file: fileNewName })
-                .subscribe({
-                    onNext: record => {
-                        res.render("edit", {
-                            id: req.params.id,
-                            work: record.get("work.title"),
-                            title: record.get("edition.title"),
-                            author: record.get("author.name"),
-                            editor: record.get("editor.name"),
-                            date: record.get("date.on"),
-                            file: record.get("file.name")
-                        });
-                    },
-                    onCompleted: () => {
-                        console.log("Data added to the graph");
-                    },
-                    onError: err => {
-                        console.log("Error related to the upload to Neo4j: " + err)
-                    }
-                })
-            );
-        } catch (err) {
-            console.log("Error related to Neo4j: " + err);
-        } finally {
-            await session.close();
-        };
+                });
+            };
+            /* post the file */
+            var fileNewName = file.name + ".html";
+            const session = driver.session();
+            try {
+                await session.writeTransaction(tx => tx
+                    .run(
+                        `
+                        MATCH (author:Author)<-[w:WRITTEN_BY]-(work:Work)-[r:HAS_MANIFESTATION]->(edition:Edition)-[e:EDITED_BY]->(editor:Editor)
+                        WHERE id(editor) = ${idEditor} AND id(editor) = ${idEditor}
+                        OPTIONAL MATCH (edition)-[p:PUBLISHED_ON]->(date:Date)
+                        MERGE (file:File {name: $file})
+                        MERGE (file)-[pr:PRODUCED_BY]->(editor)
+                        RETURN work.title, edition.title, author.name, editor.name, date.on, file.name
+                        `, { file: fileNewName })
+                    .subscribe({
+                        onNext: record => {
+                            res.render("edit", {
+                                id: req.params.id,
+                                work: record.get("work.title"),
+                                title: record.get("edition.title"),
+                                author: record.get("author.name"),
+                                editor: record.get("editor.name"),
+                                date: record.get("date.on"),
+                                file: record.get("file.name")
+                            });
+                        },
+                        onCompleted: () => {
+                            console.log("Data added to the graph");
+                        },
+                        onError: err => {
+                            console.log("Error related to the upload to Neo4j: " + err)
+                        }
+                    })
+                );
+            } catch (err) {
+                console.log("Error related to Neo4j: " + err);
+            } finally {
+                await session.close();
+            };
+        });
+        form.on("error", (err) => {
+            console.log(err);
+        })
+        form.on("end", () => {
+            console.log("File uploaded");
+        });
     });
-    form.on("error", (err) => {
-        console.log(err);
-    })
-    form.on("end", () => {
-        console.log("File uploaded");
-    });
-});
 
 module.exports = router;
