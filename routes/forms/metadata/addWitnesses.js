6b9ac2ccbd62b09cb91327d269d33359f5ea2128
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2022-12-16 16:10:34 +0100

    Add files via upload

--- routes/forms/metadata/addWitnesses.js
+++ routes/forms/metadata/addWitnesses.js
@@ -21,10 +21,54 @@ router.post("/addWitnesses/:id", async (req, res) => {
                 MATCH (author:Author)<-[:WRITTEN_BY]-(work:Work)-[:HAS_MANIFESTATION]->(edition:Edition)-[:EDITED_BY]->(editor:Editor)  
                 WHERE id(edition) = ${idEdition} AND id(editor) = ${idEditor}
                 OPTIONAL MATCH (edition)-[:PUBLISHED_ON]->(date:Date)
-                MERGE (witness:Witness {siglum: $siglum})
+                MERGE (witness:Witness {
+                    siglum: $siglum, 
+                    provenance: $provenance,
+                    collection: $collection,
+                    classmark: $classmark,
+                    antigraph: $antigraph,
+                    author: $author,
+                    title: $title,
+                    notes: $notes,
+                    language: $language,
+                    scripts: $scripts,
+                    structuralTypology: $structuralTypology,
+                    state: $state,
+                    condition: $condition,
+                    format: $format,
+                    material: $material,
+                    dimensions: $dimensions,
+                    extent: $extent,
+                    bindingDescription: $bindingDescription,
+                    date: $date,
+                    foliation: $foliation,
+                    people: $people
+                })
                 MERGE (edition)<-[:USED_IN]-(witness)
                 RETURN work.title, edition.title, author.name, editor.name, witness.siglum, date.on
-                `, { siglum: req.body.witnessSiglum }
+                `, { 
+                    siglum: req.body.witnessSiglumBase + "<sup>" + req.body.witnessSiglumSuperscript + "</sup><sub>" + req.body.witnessSiglumSubscript + "</sub>",
+                    provenance: req.body.witnessProvenance,
+                    collection: req.body.witnessCollection,
+                    classmark: req.body.witnessClassmark,
+                    antigraph: req.body.witnessAntigraph,
+                    author: req.body.witnessAuthor,
+                    title: req.body.witnessTitle,
+                    notes: req.body.witnessNotes,
+                    language: req.body.witnessLanguage,
+                    scripts: req.body.witnessScripts,
+                    structuralTypology: req.body.witnessStructuralTypology,
+                    state: req.body.witnessState,
+                    condition: req.body.witnessCondition,
+                    format: req.body.witnessFormat,
+                    material: req.body.witnessMaterial,
+                    dimensions: req.body.witnessDimensions,
+                    extent: req.body.witnessExtent,
+                    bindingDescription: req.body.witnessBindingDescription,
+                    date: req.body.witnessDate,
+                    foliation: req.body.witnessFoliation,
+                    people: req.body.witnessPeople
+                }
             )
             .subscribe({
                 onNext: () => {
