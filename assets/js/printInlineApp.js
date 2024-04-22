9105bafcdf1912f4330684e4668eb43b29ba8df5
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2024-04-22 23:19:11 +0200

    parallels

--- assets/js/printInlineApp.js
+++ assets/js/printInlineApp.js
@@ -5,30 +5,60 @@ stanzasArray.forEach((stanza) => {
     var txtStructure = stanza.getAttribute("data-text-structure");
     var txtStructureN = stanza.getAttribute("data-n");
     var appEntryStanza = document.querySelector("span[data-type='apparatus'][data-subtype='stanzaStart'][data-n='" + txtStructureN + "']");
-    
+    var parStanza = document.querySelector(".par-stanza[data-n='" + txtStructureN + "']");
+
+    /* INLINE APPARATUS */
     if (appEntryStanza) {
-        /* collapse btn */
-        var btnCollapse = document.createElement("a");
-        btnCollapse.textContent = "app.";
-        btnCollapse.setAttribute("class", "caret-xs blue-600 fs-xxs p-1");
-        btnCollapse.setAttribute("data-bs-toggle", "collapse");
-        btnCollapse.setAttribute("data-bs-target", "#" + txtStructure + "-" + txtStructureN);
-        btnCollapse.setAttribute("aria-expanded", "false");
-        stanza.append(btnCollapse);
+        /* collapse app btn */
+        var btnAppCollapse = document.createElement("a");
+        btnAppCollapse.textContent = "app.";
+        btnAppCollapse.setAttribute("class", "caret-xs blue-600 fs-xxs p-1");
+        btnAppCollapse.setAttribute("data-bs-toggle", "collapse");
+        btnAppCollapse.setAttribute("data-bs-target", "#" + txtStructure + "-" + txtStructureN);
+        btnAppCollapse.setAttribute("aria-expanded", "false");
+        stanza.append(btnAppCollapse);
 
-        /* collapsible div */
-        var collapse = document.createElement("div");
-        collapse.setAttribute("class", "collapse fs-xs p-3 border rounded bg-light-blue");
-        collapse.setAttribute("id", txtStructure + "-" + txtStructureN);
-        collapse.setAttribute("data-type", "inlineApp");
-        collapse.setAttribute("data-n", txtStructureN);
+        /* collapsible app div */
+        var collapseApp = document.createElement("div");
+        collapseApp.setAttribute("class", "collapse fs-xs p-3 border rounded bg-light-blue");
+        collapseApp.setAttribute("id", txtStructure + "-" + txtStructureN);
+        collapseApp.setAttribute("data-type", "inlineApp");
+        collapseApp.setAttribute("data-n", txtStructureN);
 
-        /* append the card to the stanza */
-        stanza.append(collapse);
+        /* append the app card to the stanza */
+        stanza.append(collapseApp);
 
         /* print data in the card */
         var appEntry = appEntryStanza.parentElement.innerHTML;
         var inlineApp = document.querySelector("div[data-type='inlineApp'][data-n='" + txtStructureN + "']");
         inlineApp.innerHTML = appEntry;
     };
+
+    /* INLINE PARALLELS */
+    if (parStanza) {
+        /* collapse parallel btn */
+        var btnParCollapse = document.createElement("a");
+        btnParCollapse.textContent = "par.";
+        btnParCollapse.setAttribute("class", "caret-xs orange-400 fs-xxs p-1");
+        btnParCollapse.setAttribute("data-bs-toggle", "collapse");
+        btnParCollapse.setAttribute("data-bs-target", "#par-" + txtStructure + "-" + txtStructureN);
+        btnParCollapse.setAttribute("aria-expanded", "false");
+        stanza.append(btnParCollapse);
+
+        /* collapsible parallel div */
+        var collapsePar = document.createElement("div");
+        collapsePar.setAttribute("class", "collapse fs-xs p-3 border rounded bg-light-orange");
+        collapsePar.setAttribute("id", "par-" + txtStructure + "-" + txtStructureN);
+        collapsePar.setAttribute("data-type", "inlinePar");
+        collapsePar.setAttribute("data-n", txtStructureN);
+
+        /* append the app card to the stanza */
+        stanza.append(collapsePar);
+
+        /* print data in the card */
+        var parallel = parStanza.closest("div[data-type='parallel'][data-subtype='parallel']").innerHTML;
+        var inlinePar = document.querySelector("div[data-type='inlinePar'][data-n='" + txtStructureN + "']");
+        inlinePar.innerHTML = parallel;
+    };
+
 });
